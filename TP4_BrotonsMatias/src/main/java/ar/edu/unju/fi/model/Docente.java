15054bff111c31bce128b9d04173202ab305a9a0
PC <PC@MATIAS>
2024-06-09 11:19:53 -0300

    creacion de clases y controller

---
+++ TP4_BrotonsMatias/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -0,0 +1,54 @@
+package ar.edu.unju.fi.model;
+
+public class Docente {
+	private Integer legajo;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private float telefono;
+	
+	public Docente(Integer legajo, String nombre, String apellido, String email, float telefono) {
+		this.legajo = legajo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+	}
+	
+	public Integer getLegajo() {
+		return legajo;
+	}
+	public void setLegajo(Integer legajo) {
+		this.legajo = legajo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public float getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(float telefono) {
+		this.telefono = telefono;
+	}
+
+	
+	
+	
+	
+
+}
---
+++ TP4_BrotonsMatias/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,69 @@
+package ar.edu.unju.fi.model;
+
+public class Materia {
+	private String codigo;
+	private String nombre;
+	private String curso;
+	private Integer cantidad;
+	private Boolean modalidad;
+	private Docente docente;
+	private Carrera carrera;
+	
+	public Materia(String codigo, String nombre, String curso, Integer cantidad, Boolean modalidad, Docente docente,
+			Carrera carrera) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.curso = curso;
+		this.cantidad = cantidad;
+		this.modalidad = modalidad;
+		this.docente = docente;
+		this.carrera = carrera;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	public Boolean getModalidad() {
+		return modalidad;
+	}
+	public void setModalidad(Boolean modalidad) {
+		this.modalidad = modalidad;
+	}
+	public Docente getDocente() {
+		return docente;
+	}
+	public void setDocente(Docente docente) {
+		this.docente = docente;
+	}
+	public Carrera getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+	
+	
+	
+	
+}
